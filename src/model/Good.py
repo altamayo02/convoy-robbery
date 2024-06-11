015f4e5e25e69cfcbcd982f8a3a82addab67a8cd
Alejandro <42224066+altamayo02@users.noreply.github.com>
2024-06-11 09:08:56 -0500

    Modeling progress

---
+++ src/model/Good.py
@@ -0,0 +1,4 @@
+class Good:
+	def __init__(self, cost: float, weight: float) -> None:
+		self.cost = cost
+		self.weight = weight
---
+++ src/model/Vehicle.py
@@ -0,0 +1,5 @@
+class Vehicle:
+	def __init__(self, attack: float, defense: float, base_speed: float) -> None:
+		self.attack = attack
+		self.defense = defense
+		self.speed = base_speed
